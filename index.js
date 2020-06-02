f5154abae20654d759002992677f131af2b5f81a
lishu gupta <lishugupta652@gmail.com>
2020-06-03 00:25:06 +0530

    with some changes

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const express = require("express");
 const app = express();
 const route = require("./routes/route");
 
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://lishugupta652:lishugupta652@firstcluster-76qkd.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("connected");
+});
+
 // const mongoose = require("mongoose");
 // mongoose.connect("mongodb://localhost/test", { useNewUrlParser: true });
 
