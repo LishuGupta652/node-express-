03d0df1d8f28d340c1c575c6feac9a6a0fec5b31
lishu gupta <lishugupta652@gmail.com>
2020-06-02 22:20:07 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,21 @@
+const PORT = process.env.PORT || 4001;
+const express = require("express");
+const app = express();
+const route = require("./routes/route");
+
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb://localhost/test", { useNewUrlParser: true });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// const db = mongoose.connection;
+// db.on("error", console.log.bind(console, "connnection error"));
+
+app.use("/user", route);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Please check UniformResourceLocator");
+});
+
+app.listen(PORT, () => console.log(`App started at PORT ${PORT}`));
---
+++ routes/route.js
@@ -0,0 +1,32 @@
+const express = require("./node_modules/express");
+
+const router = express.Router();
+
+const users = [
+  {
+    id: "1",
+    name: "lishu",
+  },
+  {
+    id: "2",
+    name: "tarun",
+  },
+  {
+    id: "3",
+    name: "guptas",
+  },
+];
+
+router.get("/", (req, res) => {
+  res.send(users);
+});
+
+router.post("/", (req, res) => {
+  res.send(req.body);
+});
+
+router.get("/new", (req, res) => {
+  res.send("Hey! u pretty new user");
+});
+
+module.exports = router;
